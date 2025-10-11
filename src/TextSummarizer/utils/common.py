bbeab6c972e5d99b6930e3c3acb6435569d70e63
AtharvTungatkar <atharvpt52@gmail.com>
2025-10-11 18:17:01 -0400

    Data Ingestion Notebook Experiment Completed

--- src/TextSummarizer/utils/common.py
+++ src/TextSummarizer/utils/common.py
@@ -33,19 +33,19 @@ def read_yaml(path_to_yaml:Path)->ConfigBox:
         raise e
     
 @ensure_annotations
- def create_directories(path_to_directories: list,verbose=True):
+def create_directories(path_to_directories: list,verbose=True):
      """create list of directories
         
     Args:
         path_to_directories (list): list of path of directories
         ignore_log(bool,optional): ignore if multiple dirs is to be created. Defaults to False"""
-    for path in path_to_directories:
+     for path in path_to_directories:
         os.makedirs(path,exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
 
 
- @ensure_annotations
+@ensure_annotations
 def get_size(path:Path)->str:
     """get size in KB
     
